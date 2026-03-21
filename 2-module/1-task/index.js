ac3dc584187bee4ade0484a2f65f1defe17986ec
Igor Pershin <igor.ps@soflex.md>
2026-03-22 01:31:22 +0200

    Added Solutions: module №2

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,26 @@
+/*
+// Объект для отладки в консоли браузера
+
+let salaries = {
+  John: 1000,
+  Ann: 1600,
+  Pete: 1300,
+  month: 'December',
+  currency: 'USD',
+  isPayed: false
+};
+*/
+
 function sumSalary(salaries) {
-  // ваш код...
+  let result = 0;
+
+  for (let salary in salaries) {
+    const value = salaries[salary];
+
+    if (Number.isFinite(value)) {
+      result += value;
+    }
+  }
+
+  return result;
 }

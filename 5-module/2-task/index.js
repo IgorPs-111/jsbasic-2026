48096d13f1a0ef962df862b6e27ce921070819cf
Igor Pershin <igor.ps@soflex.md>
2026-03-29 14:05:08 +0300

    Added Solutions: module №5

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,13 @@
 function toggleText() {
-  // ваш код...
+  const btn = document.querySelector('.toggle-text-button');
+  const text = document.getElementById('text');
+
+  btn.addEventListener('click', (e) => {
+    if (text.hasAttribute('hidden')) {
+      text.removeAttribute('hidden');
+
+    } else {
+      text.setAttribute('hidden', '');
+    }
+  });
 }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,50 @@
 function initCarousel() {
-  // ваш код...
+  const carouselInner = document.querySelector('.carousel__inner');
+  const carouselInnerWidth = carouselInner.offsetWidth;
+  const carouselSlides = document.querySelectorAll('.carousel__slide');
+
+  const btnPrev = document.querySelector('.carousel__arrow_left');
+  const btnNext = document.querySelector('.carousel__arrow_right');
+
+  let currentTranslateX = 0;
+  let currentSlideIndex = 0;
+
+  if (currentSlideIndex === 0) {
+    btnPrev.style.display = 'none';
+  }
+
+  function carouselSliderSwipe () {
+    carouselInner.style.transform = `translateX(${currentTranslateX}px)`;
+  }
+
+  function hiddenCarouselButtonsPrev() {
+    if (currentSlideIndex === 0) {
+      btnPrev.style.display = 'none';
+
+    } else if (currentSlideIndex === carouselSlides.length - 2) {
+      btnNext.style.display = 'flex';
+    }
+  }
+
+  function hiddenCarouselButtonsNext() {
+    btnPrev.style.display = 'flex';
+
+    if (currentSlideIndex === carouselSlides.length - 1) {
+      btnNext.style.display = 'none';
+    }
+  }
+
+  btnPrev.addEventListener('click', () => {
+    currentTranslateX += carouselInnerWidth;
+    carouselSliderSwipe();
+    currentSlideIndex--;
+    hiddenCarouselButtonsPrev();
+  });
+
+  btnNext.addEventListener('click', () => {
+    currentTranslateX -= carouselInnerWidth;
+    carouselSliderSwipe();
+    currentSlideIndex++;
+    hiddenCarouselButtonsNext();
+  });
 }
